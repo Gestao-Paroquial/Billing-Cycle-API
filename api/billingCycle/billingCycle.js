d1c2012ed5278d279239c554cdf438712011f318
Leonardo Cardoso <leocardosoti@gmail.com>
2018-03-18 15:46:17 -0300

    adicionando comunidade ao gasto

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -1,25 +1,32 @@
-const restful = require('node-restful')
-const mongoose = restful.mongoose
+const restful = require("node-restful");
+const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
   name: { type: String, required: true },
   value: { type: Number, min: 0, required: true }
-})
+});
 
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Informe o valor do débito!"]
+  },
   status: {
-    type: String, required: false, uppercase: true,
-    enum: ['PAGO', 'PENDENTE', 'AGENDADO']
+    type: String,
+    required: false,
+    uppercase: true,
+    enum: ["PAGO", "PENDENTE", "AGENDADO"]
   }
-})
+});
 
 const billingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   date: { type: Date, require: true },
+  comunidade_id: { type: Number, require: true },
   credits: [creditSchema],
   debts: [debtSchema]
-})
+});
 
-module.exports = restful.model('BillingCycle', billingCycleSchema)
+module.exports = restful.model("BillingCycle", billingCycleSchema);
