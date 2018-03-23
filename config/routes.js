0b2c9284bc58cd6d7e7b881e47787468b1399311
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-23 15:38:39 -0300

    adicionando rota para pegar informações do facebook

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch')
 const express = require('express')
 const billingCycleService = require('../api/billingCycle/billingCycleService')
 const billingSummaryService = require('../api/billingSummary/billingSummaryService')
@@ -11,4 +12,10 @@ module.exports = (app) => {
   billingCycleService.register(router, '/billingCycles')
 
   router.route('/billingSummary').get(billingSummaryService.getSummary)
+
+  router.route('/facebook').get((req, res, next) => {
+    fetch(`https://graph.facebook.com/v2.12/829673350416518?fields=fan_count&access_token=${process.env.FB_PAGE_TOKEN}`)
+      .then(response => response.json())
+      .then(json => res.json(json))
+  })
 }
