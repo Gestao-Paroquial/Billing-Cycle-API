a44fda48c14c6b661a72baf54b2178cdf7a09b33
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-26 18:16:38 -0300

    update

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,5 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/deploy # Allow read access to the private key
 ssh-add /tmp/deploy # Add the private key to SSH
 
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh deploy@165.227.197.233  <<EOF
-  cd ~
-  touch batata.txt # Change to whatever commands you need!
-EOF
+ssh-keyscan -p 22 -t rsa 165.227.197.233 >> ~/.ssh/known_hosts
+touch ~/teste.txt
