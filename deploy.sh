465a7e7ca87d5d706b95bb0f4d9dd1dea609bc12
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-26 18:08:42 -0300

    update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ chmod 600 /tmp/deploy # Allow read access to the private key
 ssh-add /tmp/deploy # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh deploy@165.227.197.233 -p  <<EOF
+ssh deploy@165.227.197.233  <<EOF
   cd ~
   touch batata.txt # Change to whatever commands you need!
 EOF
