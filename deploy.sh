954234ab289255c0e7376713c488e7c80c3a8ace
Leonardo Cardoso <leocardosoti@gmail.com>
2018-04-01 23:18:01 -0300

    teste travis

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,5 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/deploy # Allow read access to the private key
 ssh-add /tmp/deploy # Add the private key to SSH
 
-ssh-keyscan -p 22 -t rsa 165.227.197.233 >> ~/.ssh/known_hosts
-touch ~/teste.txt
-
-# ssh deploy@165.227.197.233 -p 22 <<EOF
-#   cd ~
-#   ~/touch batata.txt # Change to whatever commands you need!
-# EOF
-
 ssh deploy@165.227.197.233
+touch batata.txt
