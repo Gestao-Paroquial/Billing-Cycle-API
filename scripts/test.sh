8614b2967bb65c3a9e6b971c30e4f119fe815cdb
Leonardo Cardoso <leocardosoti@gmail.com>
2018-04-02 00:06:27 -0300

    update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -x
 
+export NODE_ENV=production
+export NVM_BIN=~/.nvm/versions/node/v8.9.4/bin
+
+. /root/.nvm/nvm.sh
 cd /var/www/node_api && \
-git pull origin master && \
-pm2 restart 0
+git pull origin master 
