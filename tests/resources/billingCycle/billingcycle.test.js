cbb1ad0257818b95909ed1685afd3d4db76b9a3d
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-23 20:54:11 -0300

    testing billingCycles querys

--- tests/resources/billingCycle/billingcycle.test.js
+++ tests/resources/billingCycle/billingcycle.test.js
@@ -7,27 +7,61 @@ describe('billingCycle', () => {
   const billingCycleModel = {
     name: 'teste',
     date: Date.now(),
-    credits: [],
-    debts: [],
+    credits: [{
+      name: 'teste',
+      value: 50,
+      donationId: 1,
+    }],
+    debts: [{
+      name: 'teste',
+      value: 50,
+    }],
   }
   beforeEach(async () => {
     await setupTest()
 
     await BillingCycle.insertMany([billingCycleModel, billingCycleModel])
   })
-  it('should be null when user is not logged in', async () => {
-    // language=GraphQL
-    const query = `
-    {
-      billingCycles {
-        id
+  describe('querys', () => {
+    describe('billingCycles', () => {
+      const fields = `
+      id
+      name
+      date
+      debts {
+        name
+        value 
       }
-    }
-  `
-
-    const { data: { billingCycles } } = await graphql(schema, query)
-
-    expect(billingCycles).toBeInstanceOf(Array)
-    expect(billingCycles.length).toBe(2)
+      credits {
+        name
+        value
+        donationId
+      }
+      `
+      const query = `
+        {
+          billingCycles {
+              ${fields}
+          }
+        }
+      `
+      it('should return two billingCycles', async () => {
+        const { data: { billingCycles } } = await graphql(schema, query)
+        expect(billingCycles).toBeInstanceOf(Array)
+        expect(billingCycles.length).toBe(2)
+      })
+      it('should return one billingCycle when pass a first and offset', async () => {
+        const query2 = `
+        {
+          billingCycles(first:1, offset: 0) {
+              ${fields}
+          }
+        }
+      `
+        const { data: { billingCycles } } = await graphql(schema, query2)
+        expect(billingCycles).toBeInstanceOf(Array)
+        expect(billingCycles.length).toBe(1)
+      })
+    })
   })
 })
