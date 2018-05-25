9bdbb3d47d74fea7ca166fd4462faec239e24e49
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-24 21:40:16 -0300

    call setup function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-
+const setup = require('./config/setup')
 const server = require('./config/app')
 require('./config/database')
 require('./config/routes')(server)
+
+setup()
